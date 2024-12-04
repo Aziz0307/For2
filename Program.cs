6502c5275e7d7e0aac46d1d329c7b9e401d857b4
Aziz0307 <Aziz03072018@gmail.com>
2024-12-04 18:58:41 +0500

    first coomit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace For1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int n = 0;
+            for (int i = b-1; i > a; i--)
+            {
+                n++;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Kolishestvo n takikh chislo ravno= {n}");
+        }
+    }
+}
